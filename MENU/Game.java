ea143da4699f9534c90de34f41db0e50f70b9b13
naditjandra <168246764+naditjandra@users.noreply.github.com>
2024-05-18 22:45:09 +0700

    add menu

---
+++ MENU/Game.java
@@ -0,0 +1,143 @@
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements Runnable {
+
+	private GameScreen gameScreen;
+	private Thread gameThread;
+	private States states;
+
+	private final double FPS_SET = 120.0;
+	private final double UPS_SET = 60.0;
+
+	// Classes
+	private Render render;
+	private Menu menu;
+	private PlantsList plantsList;
+	private ZombiesList zombiesList;
+	private Help help;
+	private Play play;
+	private GameOver gameOver;
+
+	public Game() {
+
+		initClasses();
+		states = States.MENU;
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setResizable(false);
+		add(gameScreen);
+		pack();
+		setVisible(true);
+
+	}
+
+	private void initClasses() {
+		render = new Render(this);
+		gameScreen = new GameScreen(this);
+		menu = new Menu(this);
+		plantsList = new PlantsList(this);
+		zombiesList = new ZombiesList(this);
+		help = new Help(this);
+		play = new Play(this);
+		gameOver = new GameOver(this);
+
+	}
+
+	private void start() {
+		gameThread = new Thread(this) {
+		};
+
+		gameThread.start();
+	}
+
+	private void updateGame() {
+
+		// System.out.println("Game Updated!");
+	}
+
+	public static void main(String[] args) {
+
+		Game game = new Game();
+		game.gameScreen.initInputs();
+		game.start();
+
+	}
+
+	@Override
+	public void run() {
+
+		double timePerFrame = 1000000000.0 / FPS_SET;
+		double timePerUpdate = 1000000000.0 / UPS_SET;
+
+		long lastFrame = System.nanoTime();
+		long lastUpdate = System.nanoTime();
+		long lastTimeCheck = System.currentTimeMillis();
+
+		int frames = 0;
+		int updates = 0;
+
+		long now;
+
+		while (true) {
+			now = System.nanoTime();
+			
+			// Render
+			if (now - lastFrame >= timePerFrame) {
+				repaint();
+				lastFrame = now;
+				frames++;
+			}
+
+			// Update
+			if (now - lastUpdate >= timePerUpdate) {
+				updateGame();
+				lastUpdate = now;
+				updates++;
+			}
+
+			if (System.currentTimeMillis() - lastTimeCheck >= 1000) {
+				System.out.println("FPS: " + frames + " | UPS: " + updates);
+				frames = 0;
+				updates = 0;
+				lastTimeCheck = System.currentTimeMillis();
+			}
+
+		}
+
+	}
+
+	// Getters and setters
+	public Render getRender() {
+		return render;
+	}
+
+	public Menu getMenu() {
+		return menu;
+	}
+
+	public PlantsList getPlantsList() {
+		return plantsList;
+	}
+
+	public ZombiesList getZombiesList() {
+		return zombiesList;
+	}
+	public Help getHelp() {
+		return help;
+	}
+	public Play getPlay() {
+		return play;
+	}
+	public GameOver getGameOver() {
+		return gameOver;
+	}
+	public States getStates() {
+		return states;
+	}
+
+	public void setStates(States states) {
+		this.states = states;
+	}
+
+}
---
+++ MENU/Menu.java
@@ -0,0 +1,145 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+
+public class Menu implements ScreenMethod {
+
+    private Game game;
+	
+
+	private MyButton bPlaying, bPlantsList, bZombiesList, bQuit, bHelp, bGameOver;
+
+	public Menu(Game game) {
+		this.game = game;
+
+		initButtons();
+	}
+
+	private void initButtons() {
+
+		int w = 100;
+		int h = 33;
+		int yOffset = 100;
+
+		bPlaying = new MyButton("Play", 280, 280, w, h);
+		bPlantsList = new MyButton("Plants List", 197, 128 + yOffset, w, h);
+        bZombiesList = new MyButton("Zombies List", 362, 28 + yOffset * 2, w, h);
+		bQuit = new MyButton("Quit", 362, 334, w, h);
+        bHelp = new MyButton("Help", 203, 334, w, h);
+		bGameOver = new MyButton("GO", 0, 0, w, h);
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+       
+        drawBackground(g);
+        drawButtons(g);
+		
+
+	}
+
+	private void drawBackground(Graphics g) {
+        BufferedImage img = null;
+		InputStream is = getClass().getResourceAsStream("MENU.png");
+
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+        g.drawImage(img, 0, 0, null);
+
+    }
+
+    private void drawButtons(Graphics g) {
+		bPlaying.draw(g);
+		bPlantsList.draw(g);
+        bZombiesList.draw(g);
+		bQuit.draw(g);
+        bHelp.draw(g);
+        bGameOver.draw(g);
+
+	}
+
+	@Override
+	public void mouseClicked(int x, int y) {
+
+		if (bPlaying.getBounds().contains(x, y)) {
+			game.setStates(States.PLAY);
+        } else if (bPlantsList.getBounds().contains(x, y)) {
+			game.setStates(States.PLANTSLIST);
+        } else if (bQuit.getBounds().contains(x, y)) {
+			System.exit(0);
+        } else if (bZombiesList.getBounds().contains(x, y)) {
+            game.setStates(States.ZOMBIESLIST);
+        } else if (bHelp.getBounds().contains(x, y)) {
+            game.setStates(States.HELP);
+		} else if (bGameOver.getBounds().contains(x, y)) {
+            game.setStates(States.GAMEOVER);
+        }
+	}
+
+	@Override
+	public void mouseMoved(int x, int y) {
+		bPlaying.setMouseOver(false);
+		bPlantsList.setMouseOver(false);
+        bZombiesList.setMouseOver(false);
+		bQuit.setMouseOver(false);
+		bHelp.setMouseOver(false);
+		bGameOver.setMouseOver(false);
+
+		if (bPlaying.getBounds().contains(x, y)) {
+			bPlaying.setMouseOver(true);
+		} else if (bPlantsList.getBounds().contains(x, y)) {
+			bPlantsList.setMouseOver(true);
+		} else if (bZombiesList.getBounds().contains(x, y)) {
+			bZombiesList.setMouseOver(true);
+		} else if (bQuit.getBounds().contains(x, y)) {
+			bQuit.setMouseOver(true);
+		} else if (bHelp.getBounds().contains(x, y)) {
+			bHelp.setMouseOver(true);
+		} else if (bGameOver.getBounds().contains(x, y)) {
+			bGameOver.setMouseOver(true);
+		}
+
+	}
+
+	@Override
+	public void mousePressed(int x, int y) {
+
+		if (bPlaying.getBounds().contains(x, y)) {
+			bPlaying.setMousePressed(true);
+		} else if (bPlantsList.getBounds().contains(x, y)) {
+			bPlantsList.setMousePressed(true);
+		} else if (bZombiesList.getBounds().contains(x, y)) {
+			bZombiesList.setMousePressed(true);
+		} else if (bQuit.getBounds().contains(x, y)) {
+			bQuit.setMousePressed(true);
+		} else if (bHelp.getBounds().contains(x, y)) {
+			bHelp.setMousePressed(true);
+		} else if (bGameOver.getBounds().contains(x, y)) {
+			bGameOver.setMousePressed(true);
+		}
+	}
+
+	@Override
+	public void mouseReleased(int x, int y) {
+		resetButtons();
+	}
+
+	private void resetButtons() {
+		bPlaying.resetBooleans();
+		bPlantsList.resetBooleans();
+        bZombiesList.resetBooleans();
+		bQuit.resetBooleans();
+        bHelp.resetBooleans();
+		bGameOver.resetBooleans();
+	}
+
+}
