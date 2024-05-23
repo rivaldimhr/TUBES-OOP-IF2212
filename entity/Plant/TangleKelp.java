3605d70a0aa47e8e0eebd7820ccd8d85afa84785
naditjandra <168246764+naditjandra@users.noreply.github.com>
2024-05-23 19:20:52 +0700

    Update

--- entity/Plant/TangleKelp.java
+++ entity/Plant/TangleKelp.java
@@ -5,33 +5,41 @@ import entity.Zombie.Zombie;
 
 public class TangleKelp extends Plant {
     private int timer;
+    private String move;
+    private  boolean first = true;
+    private final int RANGE = 120; // Range within which Squash can kill zombies
+    private boolean instantKill = false;
 
     public TangleKelp(int x, int y) {
         super("TangleKelp", 100, true, 5000, 0, 50, 1, 20, x, y,
                 "image\\TangleKelp.gif");
     }
 
-    public void attack() {
-        spawnBullet();
-    }
-
-    private void spawnBullet() {
-        Bullet bullet = new Bullet(x, y, attackDamage);
-        Bullet.bullets.add(bullet);
-    }
-
     @Override
     public void actionPerformed() {
-        for (Zombie zombie : Zombie.zombies) {
-            if (zombie.gety() == y) {
-                if (timer >= 120) {
-                    attack();
-                    timer = 0;
-                } else {
-                    timer++;
-                }
+        hitzombie();
+    }
+    
+    public void instant_kill(Zombie zombie) {
+        zombie.setHealth(0);
+    }
 
+    public void hitzombie() {
+        for (Zombie zombie : Zombie.zombies) {
+            if (x - zombie.getX() <= RANGE && y == zombie.gety() && (move=="back" || first)) {
+                zombie.takeDamage(zombie.getHealt()); // Instant kill
+                instantKill = true;
+                move="back";
+                first = false;
+            } else if (zombie.getX() - x <= RANGE && y == zombie.gety() && (move=="front" || first)) {
+                zombie.takeDamage(zombie.getHealt()); // Instant kill
+                instantKill = true;
+                move="front";
+                first = false;
             }
+        }
+        if (instantKill) {
+            this.setHealth(0);; // Squash dies after killing zombies
         }
     }
 }
